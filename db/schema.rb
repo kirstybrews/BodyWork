39dfdf4672b2a9248910541670b83c54b8753fcb
github username <github email address>
2020-12-01 11:26:06 -0600

    edit 3

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_01_161935) do
+ActiveRecord::Schema.define(version: 2020_12_01_165906) do
+
+  create_table "exercises", force: :cascade do |t|
+    t.string "name"
+    t.string "category"
+    t.string "instructions"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
