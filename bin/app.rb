e344c983bfbb51fdd3cbd4a0e99b52670ab0343e
Your Username <you@example.com>
2020-12-02 16:55:23 -0500

    edit 7

--- bin/app.rb
+++ bin/app.rb
@@ -8,8 +8,10 @@ puts "} Welcome to BodyWork! {"
 puts "~~~~~~~~~~~~~~~~~~~~~~~~~"
 puts 
 puts "Enter your Name"
-user_input = gets.chomp 
-response = User.find_by(name: "#{user_input}") 
+user_input = gets.chomp #what the user inputs
+response = User.find_name(user_input) 
+p response
+
 
 # binding.pry 
 0
--- lib/user.rb
+++ lib/user.rb
@@ -2,6 +2,17 @@ class User < ActiveRecord::Base
     has_many :records
     has_many :exercises, through: :records
 
- 
+    # def self.find_names(name)
+    #         self.all.select do |u|
+    #             if u.name == name 
+    #                 return u
+    #         end
+    #     end
+    # end 
+
+    def self.find_name(user_name)
+       user = self.find_by(name: user_name) 
+        p "Welcome #{user.name}"
+    end 
 
 end 
